5586c985a9e6427ddae5755f3661b0777c807600
Shikhar Garg <shikherbest5@gmail.com>
2022-03-06 21:25:26 +0530

    Commit-Solution

---
+++ Solution/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Button prevBtn;
+    public Button nextbtn;
+
+    private void Start()
+    {
+        prevBtn.onClick.AddListener(onPrevBtnClicked);
+        nextbtn.onClick.AddListener(onNextBtnClicked);
+    }
+
+    private void onPrevBtnClicked()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+
+    private void onNextBtnClicked()
+    {
+        if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
